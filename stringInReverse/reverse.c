7b41c25e8e948baa95bc65c834ee9061eb47afb7
Fonysony <wmdragonfire@gmail.com>
2023-04-01 20:18:40 -0500

    Working on reverse string

--- stringInReverse/reverse.c
+++ stringInReverse/reverse.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+void reverse_string(const char name[]) {
+	printf("What is your name?\n");
+
+	scanf_s("%s", name);
+
+	printf("%s", name);
+}
+
 int main()
 {
-
+	reverse_string("William");
 }
