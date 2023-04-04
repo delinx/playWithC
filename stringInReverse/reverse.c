892fc2ab052fc19a7f6ac3627c0e54d4e1854459
Fonysony <wmdragonfire@gmail.com>
2023-04-04 00:46:34 -0500

    Reverse string done!

--- stringInReverse/reverse.c
+++ stringInReverse/reverse.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void print_anything(void *ptr, size_t data_size) {
     printf("Size of the variable is %zu bytes\n", data_size);
@@ -20,15 +21,27 @@ void print_anything(void *ptr, size_t data_size) {
     }
 }
 
+char* reverseString(char name[], const size_t length) {
+    printf("%zu\n", length);
+    for (int i = 0; i < length-1; i++) {
+        printf("PHASE %i: \n", i);
+        char lastElement = name[length - 1];
+        for (int j = length - 1; j > i; j--) {
+            name[j] = name[j - 1];
+            printf("name[%d] = %c\n", j, name[j]);
+        }
+        name[i] = lastElement;
+        printf("name[%d] = %c\n", i, name[i]);
+    }
+    printf("%s\n", name);
+    return name;
+}
+
 int main()
 {
-    char age_str[10];
-    int age;
-
-    printf("Enter your age: ");
-    fgets(age_str, 10, stdin); // read up to 9 characters from the user
-    age = atoi(age_str); // convert the string to an integer
-
-    printf("Your age is: %d\n", age);
-    print_anything(&age, sizeof(age));
+    char name[5] = "Miles";
+    reverseString(name, sizeof(name));
+    for (int i = 0; i < sizeof(name); i++) {
+        printf("%c\n", name[i]);
+    }
 }
