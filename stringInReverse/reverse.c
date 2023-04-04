f5e38bbab964aad78299ec8a93460620bf879f2a
Fonysony <wmdragonfire@gmail.com>
2023-04-03 20:20:38 -0500

    Working on stringInReverse

--- stringInReverse/reverse.c
+++ stringInReverse/reverse.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 
-void reverse_string(const char name[]) {
-	printf("What is your name?\n");
+void print_anything(void *ptr, size_t data_size) {
+    printf("Size of the variable is %zu bytes\n", data_size);
 
-	scanf_s("%s", name);
-
-	printf("%s", name);
+    switch (data_size)
+    {
+    case 1:
+        printf("%c\n", *(char*)ptr);
+        break;
+    case 4:
+        printf("%d\n", *(int*)ptr);
+        break;
+    case 8:
+        printf("%f\n", *(double*)ptr);
+        break;
+    default:
+        printf("Error: print_anything() not a valid data size");
+        break;
+    }
 }
 
 int main()
 {
-	reverse_string("William");
+    char age_str[10];
+    int age;
+
+    printf("Enter your age: ");
+    fgets(age_str, 10, stdin); // read up to 9 characters from the user
+    age = atoi(age_str); // convert the string to an integer
+
+    printf("Your age is: %d\n", age);
+    print_anything(&age, sizeof(age));
 }
